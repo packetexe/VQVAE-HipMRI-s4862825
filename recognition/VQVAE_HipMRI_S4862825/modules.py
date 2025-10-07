82e3c459df1e34a015d0cdc2fea69f5dd8ce9a86
Melvin Sii <melvinsii1911@gmail.com>
2025-10-07 11:37:29 +1000

    Added empty scaffolds for residuals, encoder, decoder and VQVAE

---
+++ recognition/VQVAE_HipMRI_S4862825/modules.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Residual(nn.Module):
+    def __init__(self, ch: int):
+        super().__init__()
+        #add residual conv block
+
+    def forward(self, x):
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, in_ch=1, hidden=128, z_dim=64):
+        super().__init__()
+        #conv downsamples
+
+    def forward(self, x):
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, z_dim=64, hidden=128, out_ch=1):
+        super().__init__()
+        #conv transpose upsamples
+
+    def forward(self, z):
+        return z
+
+class VQVAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #Wire encoder/decoder and VQ block
+
+    def forward(self, x):
+        return x, torch.tensor(0.0), {}
